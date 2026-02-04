59cf003de70dedc95a6de7baa3f935cd23a37816
lnss7 <novellysimaolucas@gmail.com>
2026-02-04 09:40:37 -0300

    feat: Initialize Next.js project with core pages, UI components, and barbershop-specific public assets.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
-
-export default function Home() {
+import Header from "./_components/header"
+const Home = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Barber Shop</h1>
+    <div>
+      {/*Header*/}
+      <Header />
     </div>
-  );
+  )
 }
+
+export default Home
