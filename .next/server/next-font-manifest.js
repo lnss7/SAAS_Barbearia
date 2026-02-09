0fa57c323d3a2e00f38c26e4889270c5f087dd54
Lucas Simão <novellysimaolucas@gmail.com>
2026-02-09 18:49:15 -0300

    chore: stop tracking .next folder to reduce noise

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/lns7_/Desktop/SAAS_Barbearia/app/layout\":[\"static/media/e4af272ccee01ff0-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
